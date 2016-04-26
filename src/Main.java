9e1592c701b65cf6de764c190f7ced440b7c53fa
Pieter-Jan <pieterjan.coenen@gmail.com>
2016-04-26 15:18:44 +0200

    Read file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static void main(String[] args) throws IOException {
+		String path = args[0];
+		File input = new File("./"+path);
+		BufferedReader reader = new BufferedReader(new FileReader(input));
+		//Read file
+	    String text = null;
+	    while ((text = reader.readLine()) != null) {
+	        System.out.println(text);
+	    }
+	    reader.close();
+	} 
 
 }
