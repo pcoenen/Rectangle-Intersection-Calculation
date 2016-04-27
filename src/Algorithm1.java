4c3a8ff898ab2b2d7539e04487bd354b85e96ed1
Stijn Caerts <StijnCaerts@users.noreply.github.com>
2016-04-27 10:50:22 +0200

    dubbels eruit gefilterd

--- src/Algorithm1.java
+++ src/Algorithm1.java
@@ -9,14 +9,16 @@ public class Algorithm1 extends Algorithm {
 
 	ArrayList<double[]> run(){
 		ArrayList<double[]> intersections = new ArrayList<>();
+		HashSet<Rectangle> checkedRectangles = new HashSet<>();
 		for(Rectangle rect1 : getRechthoeken()){
 			for(Rectangle rect2 : getRechthoeken()){
 				// TODO: controleer of de snijpunten tussen rect2 en rect1 al berekend zijn, anders hebben we alle snijpunten dubbel
-				if(rect1 != rect2 && hasIntersection(rect1, rect2)){
+				if(rect1 != rect2 && !checkedRectangles.contains(rect2) && hasIntersection(rect1, rect2)){
 					ArrayList<double[]> result = getIntersectionPoints(rect1, rect2);
 					intersections.addAll(result);
 				}				
 			}
+			checkedRectangles.add(rect1);
 		}
 		// TODO: only for debug purposes
 		for (double[] intersection : intersections) {
