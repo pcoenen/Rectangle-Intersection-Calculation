4a30612f689012283a3d1bfe6195f790c47d6ed5
Pieter-Jan <pieterjan.coenen@gmail.com>
2016-05-19 11:20:06 +0200

    volledig klaar
    Former-commit-id: b32a2b7755bafca3160d2d9471a9ac16cbaf2b1c

--- src/main/Algorithm3.java
+++ src/main/Algorithm3.java
@@ -1,9 +1,5 @@
 package main;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.PriorityQueue;
 
 public class Algorithm3 extends Algorithm {
